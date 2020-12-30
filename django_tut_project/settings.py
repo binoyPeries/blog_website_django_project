81efdd5c476a698256860b6f3dbd9e8968f3bad4
binoy <binoyperies98@gmail.com>
2020-12-30 17:47:02 +0530

    security reasons

--- django_tut_project/settings.py
+++ django_tut_project/settings.py
@@ -129,5 +129,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS =True
-EMAIL_HOST_USER= 'binoyperies98@gmail.com'
-EMAIL_HOST_PASSWORD= 'ljhwabncbezlnzcn'
+EMAIL_HOST_USER= '' #email
+EMAIL_HOST_PASSWORD= '' #passowrd
